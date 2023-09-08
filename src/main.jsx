0e5c9999a70cb1ecb3d219d93b2baf82d99f381a
ikramuzzaman455173 <jakaria455173@gmail.com>
2023-09-09 03:23:48 +0600

    update file folder structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import App from './App'
-import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import './styles/index.css'
+import './styles/style.css'
+import { router } from './Routes/Route/Route'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    // children: [
-    //   {
-
-    //   }
-    // ]
-  },
-
-])
-ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router} />)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router}></RouterProvider>
+)
